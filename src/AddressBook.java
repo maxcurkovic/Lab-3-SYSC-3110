d98e847ea147eee398ce3f650501a1ac5465f7d7
Max Curkovic <deekey45@gmail.com>
2021-09-17 10:13:44 -0400

    Committed

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -18,5 +18,9 @@ public class AddressBook {
 
     public static void main(String[] args) {
         System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo();
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
